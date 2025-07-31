205a96066177d8f2da0d191f21aee9dec9a80a7e
RaghavBhargava <raghavbhargava3@gmail.com>
2025-07-31 14:53:31 +0530

    page2

--- components/navbars/SubNavbar.jsx
+++ components/navbars/SubNavbar.jsx
@@ -20,7 +20,7 @@ const SubNavbar = ({ items }) => {
     return (
         <>
             {visible && items && (
-                <section className="bg-[#fafcff] rounded-b-lg py-3 lg:py-3 block fixed mt-[9vh] lg:mt-[11.3vh] z-[9000] lg:z-[9999] w-screen shadow-lg">
+                <section className="bg-[#fafcff] rounded-b-lg py-3 lg:py-3 block fixed mt-[9vh] lg:mt-[11.3vh] z-[9000]  w-screen shadow-lg">
                     <ul className="flex flex-nowrap items-end w-full overflow-x-auto overflow-y-hidden mb-0 container scrollbar-hide">
                         {items.map((item, index) => (
                             <li key={index} className="inline-flex mx-2 shrink-0">
